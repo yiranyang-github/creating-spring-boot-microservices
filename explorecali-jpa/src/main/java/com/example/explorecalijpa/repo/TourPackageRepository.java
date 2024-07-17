42a356f4cb124cb9640d8c54a0dfd532b0d55b73
Yiran Yang <91354357+yiranyang-github@users.noreply.github.com>
2024-07-17 14:19:44 +0000

    change

---
+++ explorecali-jpa/src/main/java/com/example/explorecalijpa/repo/TourPackageRepository.java
@@ -0,0 +1,8 @@
+package com.example.explorecalijpa.repo;
+import org.springframework.data.CrudRepository;
+import org.example.explorecalijpa.model.TourPackage;
+
+public interface TourPackageRepository extends CrudRepository<TourPackage, String >{
+
+  
+}
---
+++ explorecali-jpa/src/main/java/com/example/explorecalijpa/repo/TourRepository.java
@@ -0,0 +1,8 @@
+package com.example.explorecalijpa.repo;
+import org.springframework.data.CrudRepository;
+import com.example.explorecalijpa.model.*;;
+
+public interface TourRepository extends CrudRepository<Tour, Integer>{
+
+  
+}
