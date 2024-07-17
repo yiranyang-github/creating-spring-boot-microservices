a00f42b99219cacecdecf2aa3320527feca55df3
Yiran Yang <91354357+yiranyang-github@users.noreply.github.com>
2024-07-17 15:03:08 +0000

    change

--- explorecali-jpa/src/main/java/com/example/explorecalijpa/repo/TourPackageRepository.java
+++ explorecali-jpa/src/main/java/com/example/explorecalijpa/repo/TourPackageRepository.java
@@ -5,7 +5,10 @@ import com.example.explorecalijpa.model.TourPackage;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
 
 
 public interface TourPackageRepository extends JpaRepository<TourPackage, String> {
+  Optional<TourPackage> findByName(String name);
 }
